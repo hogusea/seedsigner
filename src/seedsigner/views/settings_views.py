0786b7b73ba220acf9e5847ba2a7488991cb5b5a
hogusea <hoguhogu11@naver.com>
2026-02-12 01:16:16 +0900

    Fix Donate QR TypeError

--- src/seedsigner/views/settings_views.py
+++ src/seedsigner/views/settings_views.py
@@ -9,6 +9,7 @@ from seedsigner.models.settings import Settings, SettingsConstants, SettingsDefi
 from .view import View, Destination, MainMenuView
 
 from seedsigner.gui.screens.screen import QRDisplayScreen 
+from seedsigner.models.qr_encoder import QrEncoder
 
 
 logger = logging.getLogger(__name__)
@@ -358,16 +359,20 @@ class IOTestView(View):
 #         return Destination(SettingsMenuView)
 
 
+
 class DonateView(View):
     def run(self):
         # 형님 지갑 주소
         DONATE_ADDR = "1LNcsr7uqn2vxhBEy9hV82VGDbcJHauxZh"
-        
-        # QR 화면 호출 (가장 단순한 형태)
+
+        # 1. QR 데이터 포장 (이게 핵심!)
+        qr_encoder = QrEncoder(data=DONATE_ADDR)
+
+        # 2. 화면 띄우기 (옵션 다 빼고 순정으로)
         self.run_screen(
             QRDisplayScreen,
-            qr_data=DONATE_ADDR, 
+            qr_encoder=qr_encoder,
             title="Donate Mobick"
         )
-        
+
         return Destination(SettingsMenuView)
